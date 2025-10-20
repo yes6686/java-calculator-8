a3f517b1a7c6218f0228f0077846648dc3d27eec
안예찬 <yes490411@gmail.com>
2025-10-20 18:15:08 +0900

    feat(app): calculate sum using StringCalculator

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        int result = StringCalculator.calculate(input);
     }
 }
